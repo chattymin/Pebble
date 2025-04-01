c260ee157c6b6ba990bc00228433a2463950dbb2
MinJae Han <hanminjae131@gmail.com>
2025-04-02 02:19:45 +0900

    [FEAT] `isLikelyAscii` function for early return in ASCII strings.

--- pebble/src/main/java/com/chattymin/pebble/PebbleExt.kt
+++ pebble/src/main/java/com/chattymin/pebble/PebbleExt.kt
@@ -1,2 +1,12 @@
 package com.chattymin.pebble
 
+// 아스키 문자열일 경우 얼리리턴 용도
+private fun String.isLikelyAscii(): Boolean {
+    var i = 0
+    val len = length
+    while (i < len) {
+        if (this[i].code > 0x7F) return false
+        i++
+    }
+    return true
+}
