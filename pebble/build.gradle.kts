76d05adc622aaf5845338e30d2d9bdaeeca17c7e
Dongmin <pdm001125@naver.com>
2025-04-07 00:29:04 +0900

    publish Maven 0.0.1 🚀

--- pebble/build.gradle.kts
+++ pebble/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.vanniktech)
 }
 
 android {
@@ -40,4 +43,42 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+val pebbleVersion = "0.0.1"
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates("io.github.chattymin", "pebble", pebbleVersion)
+
+    pom {
+        name = "Pebble"
+        description = "Android Grapheme String Extensions"
+        url = "https://github.com/chattymin/Pebble"
+        inceptionYear = "2025"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "chattymin"
+                name = "chattymin"
+                email = "parkdongmin123@gmail.com"
+                url = "https://github.com/chattymin"
+            }
+        }
+
+        scm {
+            connection = "scm:git:github.com/chattymin/pebble.git"
+            developerConnection.set("scm:git:ssh://github.com:chattymin/pebble.git")
+            url = "https://github.com/chattymin/pebble"
+        }
+    }
 }
