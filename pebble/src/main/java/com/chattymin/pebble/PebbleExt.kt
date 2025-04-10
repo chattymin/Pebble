f4228ac5fdc0b09698cf5f426fc0acf19644d41c
Dongmin <pdm001125@naver.com>
2025-04-10 23:00:28 +0900

    feat isAscii && graphemeLength at CharSequence

--- pebble/src/main/java/com/chattymin/pebble/PebbleExt.kt
+++ pebble/src/main/java/com/chattymin/pebble/PebbleExt.kt
@@ -4,9 +4,6 @@ import java.text.BreakIterator
 
 /**
  * Returns the number of graphemes (user-perceived characters) in the string.
- * Unlike the simple `length` property, which counts code units, this function
- * correctly handles grapheme clusters, including emojis and combined characters.
- * It uses `BreakIterator` to determine boundaries, ensuring accuracy for complex scripts.
  */
 val String.graphemeLength: Int
     get() {
@@ -25,15 +22,44 @@ val String.graphemeLength: Int
     }
 
 /**
- * Checks if the string consists only of ASCII characters (0x00 - 0x7F).
- * This function is useful for performance optimizations, as ASCII-only
- * strings do not require complex Unicode processing.
- * If any character falls outside the ASCII range, it immediately returns `false`.
+ * Returns the number of graphemes (user-perceived characters) in the character sequence.
  */
+val CharSequence.graphemeLength: Int
+    get() {
+        if (this.isEmpty()) return 0
+        if (this.length == 1) return 1
+        if (this.isAscii()) return this.length
+
+        val iterator = localBreakIterator
+        iterator.setText(this.toString())
+
+        var count = 0
+        while (iterator.next() != BreakIterator.DONE) {
+            count++
+        }
+        return count
+    }
+
+
 private const val ASCII_MAX = 0x7F
+
+/**
+ * Returns `true` if the string consists only of ASCII characters (0x00 - 0x7F).
+ */
 private fun String.isAscii(): Boolean {
     for (input in this) {
         if (input.code > ASCII_MAX) return false
     }
     return true
 }
+
+/**
+ * Returns `true` if the char sequence consists only of ASCII characters (0x00 - 0x7F).
+ */
+private fun CharSequence.isAscii(): Boolean {
+    for (input in this) {
+        if (input.code > ASCII_MAX) return false
+    }
+    return true
+}
+
