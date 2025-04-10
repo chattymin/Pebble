ce2e08fcd87ffbd7f512c868ad913010f22980a0
Dongmin <pdm001125@naver.com>
2025-04-10 22:58:43 +0900

    change graphemeLength function to val

--- pebble/src/main/java/com/chattymin/pebble/PebbleExt.kt
+++ pebble/src/main/java/com/chattymin/pebble/PebbleExt.kt
@@ -1,19 +1,6 @@
 package com.chattymin.pebble
 
 import java.text.BreakIterator
-import java.util.Locale
-
-private val LocalBreakIterator = ThreadLocal<BreakIterator>().apply {
-    set(BreakIterator.getCharacterInstance(Locale.ROOT))
-}
-
-private val localBreakIterator: BreakIterator = LocalBreakIterator.get() ?: initBreakIterator()
-
-private fun initBreakIterator() = BreakIterator.getCharacterInstance(Locale.ROOT).also {
-    LocalBreakIterator.set(it)
-}
-
-private const val ASCII_MAX = 0x7F
 
 /**
  * Returns the number of graphemes (user-perceived characters) in the string.
@@ -21,20 +8,21 @@ private const val ASCII_MAX = 0x7F
  * correctly handles grapheme clusters, including emojis and combined characters.
  * It uses `BreakIterator` to determine boundaries, ensuring accuracy for complex scripts.
  */
-fun String.graphemeLength(): Int {
-    if (this.isEmpty()) return 0
-    if (this.length == 1) return 1
-    if (this.isAscii()) return this.length
+val String.graphemeLength: Int
+    get() {
+        if (this.isEmpty()) return 0
+        if (this.length == 1) return 1
+        if (this.isAscii()) return this.length
 
-    val iterator = localBreakIterator
-    iterator.setText(this)
+        val iterator = localBreakIterator
+        iterator.setText(this)
 
-    var count = 0
-    while (iterator.next() != BreakIterator.DONE) {
-        count++
+        var count = 0
+        while (iterator.next() != BreakIterator.DONE) {
+            count++
+        }
+        return count
     }
-    return count
-}
 
 /**
  * Checks if the string consists only of ASCII characters (0x00 - 0x7F).
@@ -42,6 +30,7 @@ fun String.graphemeLength(): Int {
  * strings do not require complex Unicode processing.
  * If any character falls outside the ASCII range, it immediately returns `false`.
  */
+private const val ASCII_MAX = 0x7F
 private fun String.isAscii(): Boolean {
     for (input in this) {
         if (input.code > ASCII_MAX) return false
